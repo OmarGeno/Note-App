8825788cc4cd1864c5759210edfed5ba3cacb42c
OmarGeno <106829277+OmarGeno@users.noreply.github.com>
2022-12-06 01:42:43 +0200

    update

--- src/components/main/Note.jsx
+++ src/components/main/Note.jsx
@@ -2,18 +2,26 @@ import { useState } from 'react'
 import './Note.css'
 
 export const Note = (props) => {
-  const [enteredNote, setEnteredNote] = useState("")
+  const [enteredNote, setEnteredNote] = useState("");
+  const [isValid, setIsValid] = useState(true);  
 
-  if(enteredNote === "") {
-    setEnteredNote("Missed your thought")
-  }
+  // if(enteredNote === "") {
+  //   setEnteredNote("Missed your thought")
+  // }
 
   const contentChangeHandler = (e) => {
     setEnteredNote(e.target.value)
+    setIsValid(true)
   }
 
   const onSubmitHandler = (e) => {
     e.preventDefault();
+
+    if(enteredNote.trim().length === 0){
+      setIsValid(false)
+      return;
+    }
+
     const data = {
       note: enteredNote,
       id: Math.floor(Math.random() * 10000).toString()
@@ -25,19 +33,24 @@ export const Note = (props) => {
 
 
   return (
-    <div className="d-flex justify-content-between w-75 mt-4">
-      <form onSubmit={onSubmitHandler} className="d-flex justify-content-between w-100">
-        <input
-          type="text"
-          name="get_text"
-          id=""
-          className="form-control m-1"
-          placeholder="Add a new note"
-          onChange={contentChangeHandler}
-        />
-        <button type="submit" className="btn btn-dark m-1">
-          Add
-        </button>
+    <div className=" w-75 mt-4">
+      <form onSubmit={onSubmitHandler} className="d-flex justify-content-between w-100 flex-column">
+        <div className='p-2'>
+          <label style={{color: !isValid ? "red" : 'black'}}>Type in your thougth</label>
+        </div>
+        <div className='d-flex justify-content-between'>
+          <input
+            style={{borderColor: !isValid ? 'red' : 'rgb(109,118,126)'}}
+            type="text"
+            name="get_text"
+            className="form-control m-1"
+            placeholder="Add a new note"
+            onChange={contentChangeHandler}
+          />
+          <button type="submit" className="btn btn-dark m-1">
+            Add
+          </button>
+        </div>
       </form>
     </div>
   )
